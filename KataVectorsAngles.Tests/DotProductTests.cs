dec2de1cb743c56f6d90266f036239103936cf59
weabreu16 <weabreu16@gmail.com>
2022-10-05 20:58:23 -0400

    fix: references

---
+++ KataVectorsAngles.Tests/DotProductTests.cs
@@ -0,0 +1,12 @@
+using KataVectorsAngles;
+
+namespace KataVectorsAngles.Tests;
+
+public class DotProductTests
+{
+    [Fact]
+    public void Should_return_2_if_1_1_dot_product_1_1()
+    {
+        KataVector vector = new KataVector(1, 1);
+    }
+}
